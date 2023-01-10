359c973241fae1fdeaa1d70fd82a611e7802a6f0
paulandreicruz <cruz.paulandrei99@gmail.com>
2023-01-11 02:39:29 +0800

    First Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class index extends PureComponent {
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
+
+export default index
